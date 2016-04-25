6ae0a40acce2d7f2ba028be4b887334585d8f759
Ryan Policheri <ryan.policheri@my.simpson.edu>
2016-04-25 15:12:24 -0500

    3 Examples(2 by Ryan, 1 by Pat) Line and Arc classes added to shape_objects

--- arcade/shape_objects.py
+++ arcade/shape_objects.py
@@ -14,7 +14,7 @@ class Shape():
 
         self.change_x = 0
         self.change_y = 0
-        self.change_angle = 0
+        self.change_tilt_angle = 0
 
     def draw(self):   
         print("Cannot draw an object of type Shape. Use the subclasses of Shape: Rectangle, etc.")
@@ -22,7 +22,7 @@ class Shape():
     def update(self):        
         self.center_x += self.change_x
         self.center_y += self.change_y
-        self.tilt_angle += self.change_angle
+        self.tilt_angle += self.change_tilt_angle
 
 
 class Rectangle(Shape):
@@ -100,7 +100,7 @@ class Text(Shape):
         self.text = text
 
     def draw(self):
-        draw_text(self.text, self.center_x, self.center_y, self.color, self.size)
+        draw_text(self.text, self.center_x, self.center_y, self.color, self.size)      
 
 
 class Triangle():
@@ -114,9 +114,6 @@ class Triangle():
 
         self.color = color
         self.border_width = border_width
-
-        self.change_x = 0
-        self.change_y = 0
 
     def draw(self):
         draw_triangle(self.first_x, self.first_y, self.second_x, self.second_y, self.third_x, self.third_y, self.color, self.border_width)
@@ -159,17 +156,81 @@ class Parabola():
 
         self.change_x = 0
         self.change_y = 0
-        self.change_angle = 0
+        self.change_tilt_angle = 0
 
     def draw(self):
+        """ 
+        #>>> import arcade
+        #>>> def on_draw(delta_time):
+        #>>>     arcade.start_render()
+        #>>>     on_draw.parabola.draw()
+        #>>>     on_draw.parabola.update()
+            
+        #>>> arcade.open_window("Drawing Example", 800, 600)
+        #>>> arcade.set_background_color(arcade.color.WHITE)
+
+        #>>> on_draw.parabola = arcade.Parabola(300, 450, 390, 50, arcade.color.INDIGO, 14)
+        #>>> on_draw.parabola.change_y = -2
+        #>>> on_draw.parabola.change_angle = 8
+        #>>> arcade.schedule(on_draw, 1 / 80)
+        #>>> arcade.quick_run(10)
+        """
         draw_parabola(self.start_x, self.start_y, self.end_x, self.height, self.color, self.border_width, self.tilt_angle)
 
     def update(self):
-        """ ###hello"""
         self.start_x += self.change_x
         self.start_y += self.change_y
         self.end_x += self.change_x
-        self.tilt_angle += self.change_angle
+        self.tilt_angle += self.change_tilt_angle
+
+class Line():
+    def __init__(self, start_x, start_y, end_x, end_y, color = arcade.color.GREEN, width = 1):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.end_x = end_x
+        self.end_y = end_y
+        self.color = color
+        self.width = width
+
+        self.change_x = 0
+        self.change_y = 0
+
+    def draw(self):
+        draw_line(self.start_x, self.start_y, self.end_x, self.end_y, self.color, self.width)
+
+    def update(self):
+        self.start_x += self.change_x
+        self.start_y += self.change_y
+        self.end_x += self.change_x
+        self.end_y += self.change_y
+
+class Arc():
+    def __init__(self, center_x, center_y, width, height, color = arcade.color.GREEN, start_angle = 0, end_angle = 180, border_width = 0, tilt_angle = 0):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.start_angle = start_angle
+        self.end_angle = end_angle
+        self.border_width = border_width
+        self.tilt_angle = tilt_angle
+
+        self.change_x = 0
+        self.change_y = 0
+        self.change_tilt_angle = 0
+        self.change_start_angle = 0
+        self.change_end_angle = 0
+
+    def draw(self):
+        draw_arc(self.center_x, self.center_y, self.width, self.height, self.color, self.start_angle, self.end_angle, self.border_width, self.tilt_angle)
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        self.tilt_angle += self.change_tilt_angle
+        self.start_angle += self.change_start_angle
+        self.end_angle += self.change_end_angle
 
 def master_draw(object):
     object.draw()
